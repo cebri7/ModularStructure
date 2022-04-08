167804a793e6f5d1519251b181a1f9fdd31cfa3c
unknown <clementcomp7@gmail.com>
2022-04-08 00:37:58 -0400

    remove unused code

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-import UserModule from '@/modules/users'
-import { registerModules } from './register-store'
-
-registerModules({
-    UserModule: UserModule
-});
 
 axios.defaults.baseURL = 'http://localhost:8000/api/'
 axios.defaults.withCredentials = true
